c524e8232294a09b469b5e5b38447f15a4e0c4e0
marcelooliveiramendes <marcelooliveiraah09@gmail.com>
2023-08-21 12:11:44 -0300

    Adicionado os cards na aba de Home

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Card.scss'
+// import { Container } from './styles';
+
+function Card() {
+  return(
+    <div className='card'>
+        TESTE
+    </div>
+  );
+}
+
+export default Card;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import './Home.scss'
+import Card from '../../components/Card';
 
 // import { Container } from './styles';
 
 function Home() {
   return (
-    <div>Teste</div>
+    <div className={'homeContainer'}>
+        <h2>Dashboard</h2>
+        <div className='content'>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    </div>
   );
 }
 
